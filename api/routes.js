eac0c5fd479fe1c48e3d0e7c1a189b18e1f82b7f
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2025-06-20 21:55:52 -0300

    feat: implementação das rotas GET e POST

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
-//Task model: id, title, content, date, status, author
-const taskList = [];
+//Task model: id, title, content, date, status, 
+const taskList = [
+  {
+    "id": "1234",
+    "title": " Aprofunda | Pretalab",
+    "content": "Encontro com a líder técnica",
+    "date": "Sáb - 10hrs",
+    "status": "Incompleta"
+  }
 
-router.get("/", (req, res) => {
-  res.send("Bem vinde ao gerenciador de tarefas!");
+];
+
+//GET
+router.get("/tasks", (req, res) => {
+  res.json(taskList);
+})
+
+//GET POR ID
+router.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  //tenho que verificar se meu array tem um id igual
+
+  const taskFound = taskList.find(task => task.id === id);
+  res.json(taskFound);
 });
+
+//POST 
+router.post("/tasks", (req, res) => {
+  const { title, content, status } = req.body;
+
+  const newTask = {
+    id: uuidv4(),
+    title: title,
+    content: content,
+    date: new Date().toLocaleDateString('pt-BR'),
+    status: status,
+  };
+
+  taskList.push(newTask);
+
+  res
+    .status(201)
+    .json({ message: `A tarefa: ${title} foi criada com sucesso!`, newTask });
+
+});
+
+//PUT 
+
+
+//DELETE
+
 
 module.exports = router;
