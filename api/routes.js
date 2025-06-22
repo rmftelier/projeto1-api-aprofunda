62f4dfc1e4209051c8e72ba79206b83f7b99ecc3
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2025-06-21 21:11:31 -0300

    refactor: refatorando as rotas da api

--- api/routes.js
+++ api/routes.js
@@ -2,83 +2,74 @@ const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 
-//Task model: id, title, content, date, status, 
-const taskList = [
-  {
-    "id": "1234",
-    "title": " Aprofunda | Pretalab",
-    "content": "Encontro com a líder técnica",
-    "date": "Sáb - 10hrs",
-    "status": "Incompleta"
-  }
+//Task model: id, title, content, date, status 
+const taskList = [];
 
-];
-
-//GET
 router.get("/tasks", (req, res) => {
   res.json(taskList);
 })
 
-//GET POR ID -- melhorar aqui
 router.get("/tasks/:id", (req, res) => {
   const { id } = req.params;
-  //tenho que verificar se meu array tem um id igual
 
   const taskFound = taskList.find(task => task.id === id);
+
+  if (!taskFound) {
+    return res.status(404).json({ error: "Tarefa não encontrada." });
+  }
+
   res.json(taskFound);
 });
 
-//POST 
 router.post("/tasks", (req, res) => {
   const { title, content, status } = req.body;
 
   const newTask = {
     id: uuidv4(),
-    title: title,
-    content: content,
+    title,
+    content,
     date: new Date().toLocaleDateString('pt-BR'),
-    status: status,
+    status
   };
 
   taskList.push(newTask);
 
-  res
-    .status(201)
-    .json({ message: `A tarefa: ${title} foi criada com sucesso!`, newTask });
+  res.status(201)
+    .json({ message: `A tarefa com o título: ${title} foi criada com sucesso!`, task: newTask });
 
 });
 
-//PUT 
 router.put("/tasks/:id", (req, res) => {
   const { id } = req.params;
   const { title, content, status } = req.body;
 
-  const taskFound = taskList.find(task => task.id === id);
-
-  if (title != null) {
-    taskFound.title = title;
-  }
-  if (content != null) {
-    taskFound.status = status;
-  };
-
-  res.json({ message: `Tarefa com o ${id} foi atualizada`, taskFound });
-
-})
-
-
-//DELETE
-router.delete("/tasks/:id", (req, res) => {
-  const { id } = req.params;
-
-  //encontrando o indíce da task 
   const index = taskList.findIndex(task => task.id === id);
 
   if (index === -1) {
     return res.status(404).json({ error: "Tarefa não encontrada." });
   }
 
-  //excluindo
+  taskList[index] = {
+    ...taskList[index],
+    title,
+    content,
+    date: new Date().toLocaleDateString('pt-BR'),
+    status,
+  }
+
+  res.json({ message: `Tarefa com o id: ${id} foi atualizada.`, task: taskList[index] });
+
+})
+
+router.delete("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = taskList.findIndex(task => task.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Tarefa não encontrada." });
+  }
+
   taskList.splice(index, 1);
 
   return res.status(204).send();
